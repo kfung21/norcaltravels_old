2620d8a1f5e75e5971685c4fc27c16ad738adc4c
Kevin <kevtfung@gmail.com>
2020-06-23 23:37:21 -0700

    'fixed-hidden-tag'

--- doc/.vuepress/components/TagList.vue
+++ doc/.vuepress/components/TagList.vue
@@ -30,7 +30,7 @@ export default {
       for (let page of this.$site.pages) {
         for (let index in page.frontmatter.tags) {
           const tag = page.frontmatter.tags[index]
-          if (tag in tags && page.frontmatter.hidden==false) {
+          if (tag in tags && page.frontmatter.hidden==true) {
             tags[tag].push(page)
           } else {
             tags[tag] = [page]
